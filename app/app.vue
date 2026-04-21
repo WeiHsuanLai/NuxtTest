3961594181a85599ff11f2808997038d5c26488b
leolai <leolai@sunsky.com.tw>
2026-04-21 16:53:21 +0800

    移除多餘的定義，新增套件，設定啟動時自動打開網頁

--- app/app.vue
+++ app/app.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-// 使用 TypeScript 定義變數類型
-const title: string = '歡迎來到 Nuxt 專案'
 </script>
 
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1>{{ title }}</h1>
     <NuxtWelcome />
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-family: sans-serif;
-  text-align: center;
-  color: #00dc82;
-}
+
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  devServer: {
-    open: true
-  },
-
   modules: [
+    '@nuxt/ui',
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
     '@pinia/nuxt',
     '@nuxt/icon',
     '@nuxt/eslint'
-  ]
+  ],
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+        '@tanstack/vue-query'
+      ]
+    }
+  }
 })
